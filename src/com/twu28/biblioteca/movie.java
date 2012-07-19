9f6329c3d7a7ef9370a05abe9e6f158403eb57c5
nittin4664 <nittin4664@gmail.com>
2012-07-19 21:28:18 +0530

    test movie

---
+++ src/com/twu28/biblioteca/movie.java
@@ -0,0 +1,23 @@
+package com.twu28.biblioteca;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: NITIN
+ * Date: 7/19/12
+ * Time: 9:24 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class movie {
+    String name;
+    String director;
+    String rating;
+    public movie(String a, String b, String c) {
+        name=a;
+        director=b;
+        rating=c;
+    }
+
+    public String getname() {
+        return name;
+    }
+}
---
+++ test/com/twu28/biblioteca/testmovie.java
@@ -0,0 +1,23 @@
+package com.twu28.biblioteca;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: NITIN
+ * Date: 7/19/12
+ * Time: 9:22 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class testmovie {
+
+    @Test
+    //to test the display movie function
+    public void testgetname()
+    {
+        assertEquals("a",new movie("a","b","1").getname());
+    }
+
+}
